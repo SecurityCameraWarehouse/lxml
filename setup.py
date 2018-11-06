d3ea632d84d3e7f91098bf166dc66836d1de1533
Bryan Killian <bryan.killian@security-camera-warehouse.com>
2018-11-05 20:25:36 -0500

    no requirements, remove some install_reqs and dependencies

--- setup.py
+++ setup.py
@@ -1,14 +1,4 @@
-from setuptools import setup, find_packages
-import sys
-
-
-with open('requirements.txt') as f:
-    reqs = f.read()
-
-reqs = reqs.strip().split('\n')
-
-install = [req for req in reqs if not req.startswith("git+git://")]
-depends = [req.replace("git+git://", "git+http://") for req in reqs if req.startswith("git+git://")]
+from setuptools import setup
 
 setup(
     name='lxml',
@@ -18,7 +8,5 @@ setup(
     license=license,
     python_requires='>=3.6',
     package_dir={'lxml': 'lxml'},
-    packages=['lxml'],
-    install_requires=install,
-    dependency_links=depends,
+    packages=['lxml']
 )
